3d799b6d767445041721a36f2ef044bb31002e5e
Martin Wilz <github@wilz.de>
2014-05-17 21:26:09 +0200

    added lines between buttons

--- Classes/MWButtonGroup.h
+++ Classes/MWButtonGroup.h
@@ -11,13 +11,18 @@
  */
 @interface MWButtonGroup : UIView
 
+/** readonly property for accessing the array of buttons */
 @property (strong, nonatomic,readonly)  NSArray *buttons;
+
+/** textColor of the button labels. TODO: custom setter to update buttons already created*/
 @property (strong, nonatomic)           UIColor *textColor;
+
+/** property for determining the selection pattern */
+@property (assign)                      BOOL    multiSelectAllowed;
 
 /** method for replacing the current buttons by giving a list of titles. buttons are text only in this case
  @param titles  an NSArray of strings
  */
 - (void)addButtonsForTitles:(NSArray *)titles;
-
 
 @end
--- Classes/MWButtonGroup.m
+++ Classes/MWButtonGroup.m
@@ -9,6 +9,13 @@
 #import <UIKit/UIKit.h>
 #import "MWButtonGroup.h"
 
+@interface MWButtonGroup()
+
+// views used for separator lines, these are all one pixel wide
+@property (strong, nonatomic) NSArray *lineViews;
+
+@end
+
 @implementation MWButtonGroup
 
 - (id)initWithFrame:(CGRect)frame
@@ -39,6 +46,8 @@
 {
     _buttons = @[];
 
+    self.clipsToBounds = YES;
+
     self.layer.cornerRadius = 8;
     self.layer.borderColor = [UIColor whiteColor].CGColor;
     self.layer.borderWidth = 1;
@@ -50,6 +59,7 @@
 - (void)addButtonsForTitles:(NSArray *)titles
 {
     NSMutableArray *buttons = [NSMutableArray new];
+    NSMutableArray *lineViews = [NSMutableArray new];
 
     for (NSString *title in titles) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
@@ -57,8 +67,14 @@
         [button setTitleColor:self.textColor forState:UIControlStateNormal];
         [buttons addObject:button];
         [self addSubview:button];
+
+        UIView *lineView = [UIView new];
+        lineView.backgroundColor = [UIColor whiteColor];
+        [lineViews addObject:lineView];
+        [self addSubview:lineView];
     }
 
+    _lineViews = [NSArray arrayWithArray:lineViews];
     _buttons = [NSArray arrayWithArray:buttons];
 }
 
@@ -68,14 +84,24 @@
     [super layoutSubviews];
 
     CGRect buttonFrame = self.bounds;
-
     buttonFrame.size.width = ((self.frame.size.width - 1) / self.buttons.count) - self.buttons.count;
+
+    CGRect lineFrame = self.bounds;
+    lineFrame.size.width = 1;
 
     CGFloat x = 1;
 
-    for (UIButton *button in self.buttons) {
+    for (NSUInteger i = 0; i < _buttons.count; i++) {
+        UIButton *button = _buttons[i];
         buttonFrame.origin.x = x;
         button.frame = buttonFrame;
+
+        if (i < _buttons.count) {
+            UIView *lineView = _lineViews[i];
+            lineFrame.origin.x = x - 1;
+            lineView.frame = lineFrame;
+        }
+
         x += buttonFrame.size.width + 1;
     }
 }
